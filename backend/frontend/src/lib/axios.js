709a4eb5f73b991062cc9d8336f8a140fafbfb5d
Kamran Ansari <karanansari9919@gmail.com>
2025-03-29 01:09:00 +0530

    soket

--- backend/frontend/src/lib/axios.js
+++ backend/frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chat-app-jju5.onrender.com",
+    baseURL: "https://chat-app-jju5.onrender.com/api",
     withCredentials: true,
-})     
+});
